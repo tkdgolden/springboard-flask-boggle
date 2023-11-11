03b4b5725236ef38516a925621b75bd318624554
Olivia Golden <olivia.golden.w@gmail.com>
2023-11-11 13:30:33 -0600

    refactored js into smaller functions

--- static/script.js
+++ static/script.js
@@ -23,32 +23,12 @@ $submitButton.on("click", async function(evt){
     }
 })
 
-// $newGameButton.on("click", startGame)
-
-// function startGame() {
-
-// }
-
 $(function() {
-    setTimeout(async function(){
-        clearInterval(intervalId)
-        $guessInput.attr("disabled", 'disabled')
-        $submitButton.toggleClass("btn-primary")
-        $submitButton.toggleClass("btn-outline-primary")
-        $newGameButton.toggleClass("btn-primary")
-        $newGameButton.toggleClass("btn-outline-primary")
-        $timer.text("Time's Up!")
-        newLeaderboard = await axios.get("/update_leaderboard")
-        console.log(newLeaderboard)
-        $gamesPlayed.text(newLeaderboard.data["games_played"])
-        $highScore.text(newLeaderboard.data["high_score"])
+    setTimeout(function(){
+        gameOver(intervalId)
     }, 60000)
-    
-    $board.removeAttr("hidden")
-    $results.removeAttr("hidden")
     $guessInput.removeAttr("disabled")
-    $newGameButton.toggleClass("btn-primary")
-    $newGameButton.toggleClass("btn-outline-primary")
+    toggleNewGameButton()
     let time = 60
     $timer.text(time)
     let intervalId = setInterval(function(){
@@ -56,3 +36,25 @@ $(function() {
         $timer.text(time)
     }, 1000)
 })
+
+function toggleNewGameButton() {
+    $newGameButton.toggleClass("btn-primary")
+    $newGameButton.toggleClass("btn-outline-primary")
+}
+
+function gameOver(intervalId) {
+    clearInterval(intervalId)
+    $guessInput.attr("disabled", 'disabled')
+    $submitButton.toggleClass("btn-primary")
+    $submitButton.toggleClass("btn-outline-primary")
+    toggleNewGameButton()
+    $timer.text("Time's Up!")
+    updateLeaderboard()
+}
+
+async function updateLeaderboard() {
+    newLeaderboard = await axios.get("/update_leaderboard")
+    console.log(newLeaderboard)
+    $gamesPlayed.text(newLeaderboard.data["games_played"])
+    $highScore.text(newLeaderboard.data["high_score"])
+}
