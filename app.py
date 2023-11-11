f97c532525a290d3dbfe3159f94a1e4cc94790e9
Olivia Golden <olivia.golden.w@gmail.com>
2023-11-10 23:09:11 -0600

    hit save...

--- app.py
+++ app.py
@@ -1,3 +1,45 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, redirect, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+
+@app.route("/")
+def index(): 
+    session["high_score"] = 0
+    session["games_played"] = 0
+    session["board"] = boggle_game.make_board()
+    session["score"] = 0
+    session["guesses"] = []
+    return render_template("index.html", board=session["board"])
+
+@app.route("/check")
+def check():
+    guess = request.args.get("guess")
+    response = boggle_game.check_valid_word(session["board"], guess)
+    if response == "ok":
+        if guess not in session["guesses"]:
+            guesses = session["guesses"]
+            guesses.append(guess)
+            session["guesses"] = guesses
+            session["score"] += len(guess)
+        else:
+            response = "duplicate-word"
+    json_response = jsonify({"result": response, "score": session["score"]})
+    return json_response
+
+@app.route("/update_leaderboard")
+def update_leaderboard():
+    session["games_played"] += 1
+    final_score = session["score"]
+    high_score = session["high_score"]
+    if (final_score > high_score):
+        session["high_score"] = final_score
+    json_response = jsonify({"games_played": session["games_played"], "high_score": session["high_score"]})
+    return json_response
---
+++ static/script.js
@@ -0,0 +1,53 @@
+$submitButton = $("#submit")
+$guessInput = $("#guess")
+$words = $("#words")
+$score = $("#score")
+$newGameButton = $("#new")
+$board = $("#board")
+$results = $("#results")
+$timer = $("#timer")
+$highScore = $("#high_score")
+$gamesPlayed = $("#games_played")
+
+$submitButton.on("click", async function(evt){
+    evt.preventDefault()
+    guess = $guessInput[0].value
+    $guessInput[0].value = ""
+
+    let result = await axios.get("/check", {params: {guess: guess}})
+    if (result.data["result"] == "ok"){
+        $words.append(`<ul>${guess}</ul>`)
+        $score.text(`Score: ${result.data["score"]}`)
+    } else {
+        alert(guess + " is " + result.data["result"])
+    }
+})
+
+$newGameButton.on("click", startGame)
+
+function startGame() {
+    setTimeout(async function(){
+        clearInterval(intervalId)
+        $guessInput.attr("disabled", 'disabled')
+        $submitButton.toggleClass("btn-primary")
+        $submitButton.toggleClass("btn-outline-primary")
+        $newGameButton.toggleClass("btn-primary")
+        $newGameButton.toggleClass("btn-outline-primary")
+        $timer.text("Time's Up!")
+        newLeaderboard = await axios.get("/update_leaderboard")
+        console.log(newLeaderboard)
+        $gamesPlayed.text(newLeaderboard.data["games_played"])
+        $highScore.text(newLeaderboard.data["high_score"])
+    }, 60000)
+    $board.removeAttr("hidden")
+    $results.removeAttr("hidden")
+    $guessInput.removeAttr("disabled")
+    $newGameButton.toggleClass("btn-primary")
+    $newGameButton.toggleClass("btn-outline-primary")
+    let time = 60
+    $timer.text(time)
+    let intervalId = setInterval(function(){
+        time -= 1
+        $timer.text(time)
+    }, 1000)
+}
